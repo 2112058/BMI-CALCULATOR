5cea12072bc6027f664c8f1a085ffc5d3451c1cf
2112058 <136875445+2112058@users.noreply.github.com>
2023-06-24 09:44:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function submit(){
+var a=document.getElementById("h").value;
+var b=document.getElementById("w").value;
+var c=(parseFloat(a)*parseFloat(a))/10000;
+c=parseFloat(b)/c;
+c=c.toFixed(2);
+document.getElementById("screen1").innerHTML=c;
+}
